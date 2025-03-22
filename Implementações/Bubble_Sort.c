5c060f9152c6861935936db8d50a23b8ee85cc79
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2025-03-22 10:11:18 -0300

    Update Bubble_Sort.c

--- Implementações/Bubble_Sort.c
+++ Implementações/Bubble_Sort.c
@@ -23,7 +23,7 @@ void bubbleSort(int *arr, int n){
         swapped = false;
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
-                _swap(&arr[j], &arr[j + 1]);
+                _swap(arr + j, arr + j + 1);
                 swapped = true;
             }
         }
