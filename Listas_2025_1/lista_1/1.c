d12718d0787a1fed3bdc4605a631f5e287e0fcff
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2025-04-04 10:45:08 -0300

    Update 1.c

--- Listas_2025_1/lista_1/1.c
+++ Listas_2025_1/lista_1/1.c
@@ -5,30 +5,35 @@
 #include <string.h>
 
 #define MOD 100
-#define FOR(arr, n) for (int i = 0;i < n;++i)
-
-void remover_duplicatas(int *, int, int *, int *);
+#define FOR(n) for (int i = 0;i < n;++i)
 
 int main() {
 
+    void remover_duplicatas(int *vetor, int n, int *compactado, int *tam_compactado);
+
     srand((unsigned)time(NULL));
 
-    int n = rand() % MOD, arr[n];
+    int n = rand() % MOD + 1, arr[n];
 
-    FOR(arr, n) *(arr + i) = (rand() ^ 1) % MOD;
+    FOR(n) *(arr + i) = (rand() ^ 1) % MOD;
 
     int narr[n], nsize = 0;
 
+    printf("Antes:\n");
+    FOR(n) printf("|%d|", *(arr + i));
+
     remover_duplicatas(arr, n, narr, &nsize);
 
-    FOR(arr, n) printf("%d\n", *(arr + i));
+    printf("\n\n");
+    printf("Depois:\n");
+    FOR(nsize) printf("|%d|", *(narr + i));
 
     return 0;
 }
 
 void remover_duplicatas(int *vetor, int n, int *compactado, int *tam_compactado) {
     bool hsh[MOD]; memset(hsh, false, sizeof(hsh));
-    FOR(vetor, n) {
+    FOR(n) {
         if (hsh[*(vetor + i)]) continue;
         hsh[*(vetor + i)] ^= 1;
         *(compactado + (*tam_compactado)++) = *(vetor + i);
