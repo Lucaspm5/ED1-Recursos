5177bb5da967937c2fa2642a7c9d0c5b258db271
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2025-04-05 23:05:53 -0300

    Update array_manipulation.c

--- Treinos/array_manipulation.c
+++ Treinos/array_manipulation.c
@@ -6,43 +6,62 @@
 #define C 12345
 #define M 2147483648
 
-typedef struct {
-    int *beginPtr, *advancePtr, *endPtr;
-} Array;
+typedef struct { int *beginPtr, *advancePtr, *endPtr; } Array;
 
-unsigned _rand(unsigned *seed) {
-    *seed = (A * *seed + C) % M;
-    return *seed;
+typedef struct { unsigned seed; } Random;
+
+Random init_random() {
+    Random r;
+    r.seed = (unsigned)time(NULL);
+    return r;
 }
 
-int main() {
-    unsigned seed = (unsigned)time(NULL);
-    
+Array init_array() {
     Array a;
-    
     a.beginPtr = malloc(10 * sizeof(int));
-    
     if (!a.beginPtr) {
         printf("Erro ao alocar memória!\n");
         exit(EXIT_FAILURE);
     }
-
     a.advancePtr = a.beginPtr;
     a.endPtr = a.beginPtr + 10;
+    return a;
+}
 
-    while (a.advancePtr != a.endPtr) {
-        *(a.advancePtr) = (_rand(&seed) & 0x7F) | 0x1;
-        if (*(a.advancePtr) >= 100) *(a.advancePtr) %= 100;
-        a.advancePtr++;
+void free_array(Array a) {
+    free(a.beginPtr);
+}
+
+unsigned _rand(Random *r) {
+    r->seed = (A * r->seed + C) % M;
+    return (r->seed & 0x7F) | 0x1;
+}
+
+void safe_array(Array *a, Random *r) {
+    a->advancePtr = a->beginPtr;
+    while (a->advancePtr < a->endPtr) {
+        *(a->advancePtr) = _rand(r);
+        if (*(a->advancePtr) >= 100) *(a->advancePtr) %= 100;
+        a->advancePtr++;
     }
-    
+}
+
+void print_array(Array a) {
     a.advancePtr = a.beginPtr;
-    while (a.advancePtr != a.endPtr) {
+    while (a.advancePtr < a.endPtr) {
         printf("%d ", *(a.advancePtr));
         a.advancePtr++;
     }
+    printf("\n");
+}
 
-    free(a.beginPtr);
-
+int main() {
+    Random r = init_random();
+    Array a = init_array();
+    
+    safe_array(&a, &r);
+    print_array(a);
+    
+    free_array(a);
     return 0;
 }
