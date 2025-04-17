ecc4cfba5188e957eff53d19595b2325e72e9d2b
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2025-04-17 14:22:21 -0300

    Create priority_queue.h

---
+++ Implementações/maxHeap/priority_queue.h
@@ -0,0 +1,21 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+typedef struct {
+    int *heap;
+    int size;
+    int capacity;
+} Heap;
+
+int parent(int i);
+int left(int i);
+int right(int i);
+bool construtorHeap(Heap *v);
+int sizeHeap(Heap *v);
+void swap(int *a, int *b);  
+bool insert(Heap *v, int element);
+bool isEmpty(Heap *v);
+int top(Heap *v);
+int extract_max(Heap *v);
+
+#endif
